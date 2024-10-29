49c9e6dec9a2f387bef783ec3deb31b4aa70b7d0
Himateja2001 <ghimateja2001@gmail.com>
2024-10-29 13:38:15 +0530

    Commit

---
+++ src/test/java/org/example/Demo.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+    @Test
+    public void method1(){
+        System.out.println("Test2 is successful");
+    }
+}
