365f3d1d4129a937142fb3eb041353d1864d013b
Himateja2001 <ghimateja2001@gmail.com>
2024-10-29 13:35:49 +0530

    Commit

--- src/test/java/org/example/HelloPrint.java
+++ src/test/java/org/example/HelloPrint.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.junit.Test;
+
 public class HelloPrint {
-    public static void main(String[] args)
+    @Test
+    public void test()
     {
         System.out.println("Hello World");
     }
