22610a82ad57c5ad02b5624269e74e588ebd18a4
Himateja2001 <ghimateja2001@gmail.com>
2024-10-30 20:46:11 +0530

    Committed

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin={"pretty"},features = "C:\\Users\\gunth\\Documents\\TestApplication\\TestApp\\src\\test\\resources\\Features\\Login.feature", glue={"StepDefinitions"})
+@CucumberOptions(plugin={"pretty"},features="C:\\Users\\gunth\\Documents\\TestApplication\\TestApp\\src\\test\\resources", glue={"StepDefinitions"})
 public class TestRunner
 {
 
