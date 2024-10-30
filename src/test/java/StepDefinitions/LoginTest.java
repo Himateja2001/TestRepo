351f9b848b9a2cb339b5eb885185dbac9a335db0
Himateja2001 <ghimateja2001@gmail.com>
2024-10-30 18:31:37 +0530

    Committed

--- src/test/java/StepDefinitions/LoginTest.java
+++ src/test/java/StepDefinitions/LoginTest.java
@@ -3,6 +3,7 @@ package StepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,7 +15,8 @@ public class LoginTest {
     @Test
     @Given("User is on login page")
     public void userIsOnLoginPage() {
-        driver= new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
         driver.get("https://www.saucedemo.com/v1/");
     }
     @Test
